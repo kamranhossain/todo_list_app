e1c719927c20161dc438bd6330622515567dfe75
kamranhossain <kamran.hossain227@gmail.com>
2020-06-21 08:10:46 +0600

    create a graphql object todo_item

--- lib/todo_list_app_web/api/schema.ex
+++ lib/todo_list_app_web/api/schema.ex
@@ -1,6 +1,17 @@
 defmodule TodoListWeb.Api.Schema do
   use Absinthe.Schema
 
+  object :todo_item do
+    field :id, non_null(:id)
+    field :content, non_null(:string)
+
+    field :is_completed, non_null(:boolean) do
+      resolve(fn %{completed_at: completed_at}, _, _ ->
+        {:ok, !is_nil(completed_at)}
+      end)
+    end
+  end
+
   query do
     field :hello, :string do
       resolve fn _, _ ->
