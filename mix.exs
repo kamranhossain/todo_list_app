d25144429e1e13c73619b3a8f6051cf502a9bf87
kamranhossain <kamran.hossain227@gmail.com>
2020-06-21 07:51:44 +0600

    install absinthe and absinthe_plug

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule TodoListApp.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.5"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
