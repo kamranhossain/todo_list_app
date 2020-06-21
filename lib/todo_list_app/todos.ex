797cd2eba9d767b4b7502fd043bb57e62a6d123b
kamranhossain <kamran.hossain227@gmail.com>
2020-06-21 23:36:32 +0600

    create toggle_item and toggle_item_by_id func

--- lib/todo_list_app/todos.ex
+++ lib/todo_list_app/todos.ex
@@ -101,4 +101,23 @@ defmodule TodoListApp.Todos do
   def change_item(%Item{} = item, attrs \\ %{}) do
     Item.changeset(item, attrs)
   end
+
+
+
+  def toggle_item(%Item{completed_at: nil} = item) do
+    update_item(item, %{completed_at: DateTime.utc_now()})
+  end
+
+  def toggle_item(%Item{} = item) do
+    update_item(item, %{completed_at: nil})
+  end
+
+  def toggle_item_by_id(todo_item_id) when is_binary(todo_item_id) or is_integer(todo_item_id) do
+    case Repo.get(Item, todo_item_id) do
+      nil ->
+        {:ok, nil}
+      %Item{} = item ->
+        toggle_item(item)
+    end
+  end
 end
