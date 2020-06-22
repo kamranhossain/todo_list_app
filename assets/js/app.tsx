4fdf5c35c431225cf01c5640b3b068258339ef49
kamranhossain <kamran.hossain227@gmail.com>
2020-06-22 13:10:13 +0600

    rewrite for react, react dom rendering in app.tsx

--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -1,15 +1,9 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-import "../css/app.scss"
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoApp from "./TodoApp";
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import deps with the dep name or local files with a relative path, for example:
-//
-//     import {Socket} from "phoenix"
-//     import socket from "./socket"
-//
-import "phoenix_html"
+document.addEventListener("DOMContentLoaded", ()=> {
+    const container = document.getElementById("TodoApp");
+    if (!container) return;
+    ReactDOM.render(<TodoApp />, container);
+})
