1052fa34110c627cf6fb190826313119e42fe38d
kamranhossain <kamran.hossain227@gmail.com>
2020-06-22 15:10:38 +0600

    add classNames' in TodoList component for applying todolist design

--- assets/js/TodoApp/TodoList.tsx
+++ assets/js/TodoApp/TodoList.tsx
@@ -23,11 +23,11 @@ const TodoList = () => {
     `)
 
     return( 
-    <div>
-        <h3>Todo Items</h3>
-        <ul>
+    <div className="todo_list">
+        <h3 className="todo_list__header">Todo Items</h3>
+        <ul className="todo_list__list">
             { data?.todoItems ? data.todoItems.map(Item => (
-                <li key={Item.id} className={Item.isCompleted? "completed" : ""}>
+                <li key={Item.id} className={Item.isCompleted? "todo_list__item todo_list__item--completed" : "todo_list__item"}>
                     {Item.content}
                 </li>
             ))
