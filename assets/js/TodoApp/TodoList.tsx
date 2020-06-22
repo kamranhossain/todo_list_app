45889afa6cdf6f0c302674c78b40f5d9714c4c4a
kamranhossain <kamran.hossain227@gmail.com>
2020-06-23 04:47:20 +0600

    import and render the TodoListItem component in TodoList component

--- assets/js/TodoApp/TodoList.tsx
+++ assets/js/TodoApp/TodoList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 import TodoItem from "./types/TodoItem";
+import TodoListItem from "./TodoListItem";
 
 interface TodoItemQueryResult {
   todoItems: TodoItem[];
@@ -21,22 +22,11 @@ const TodoList = () => {
   return (
     <div className="todo_list">
       <h3 className="todo_list__header">Todo Items</h3>
-      <ul className="todo_list__list">
-        {data?.todoItems
-          ? data.todoItems.map((Item) => (
-              <li
-                key={Item.id}
-                className={
-                  Item.isCompleted
-                    ? "todo_list__item todo_list__item--completed"
-                    : "todo_list__item"
-                }
-              >
-                {Item.content}
-              </li>
-            ))
-          : null}
-      </ul>
+      <div className="todo_list__list">
+        {data?.todoItems?.map((item: TodoItem) => (
+          <TodoListItem key={item.id} {...item} />
+        ))}
+      </div>
     </div>
   );
 };
