b32d3fb157f536d032e37dfe290b47ee79d0e39b
kamranhossain <kamran.hossain227@gmail.com>
2020-06-23 06:10:03 +0600

    import useCallback, const toggleItem mutation and handleToggle event

--- assets/js/TodoApp/TodoListItem.tsx
+++ assets/js/TodoApp/TodoListItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import TodoItem from "./types/TodoItem";
 import { useMutation } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
@@ -13,6 +13,11 @@ const TOGGLE_TODO_ITEM = gql`
 `;
 
 const TodoListItem = ({ id, content, isCompleted }: TodoItem) => {
+  const [toggleItem] = useMutation(TOGGLE_TODO_ITEM);
+  const handleToggle = useCallback(() => {
+    toggleItem({ variables: { id } });
+  }, [id, toggleItem]);
+
   return (
     <div className="todo_item">
       <button
