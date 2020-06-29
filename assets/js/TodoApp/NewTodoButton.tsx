7438aea701dcdf9cc629c3aec04a6cb8d9b2f56b
kamranhossain <kamran.hossain227@gmail.com>
2020-06-29 17:48:01 +0600

    create a NewTodoButton component

---
+++ assets/js/TodoApp/NewTodoButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+interface Props {}
+
+const NewTodoButton = ({}: Props) => {
+  return <button className="new_todo_button">New Reminder</button>;
+};
+
+export default NewTodoButton;
