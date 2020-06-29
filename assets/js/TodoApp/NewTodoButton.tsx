316cf354e01f6a1e94aea3bb1de7ddb5f8c3ddc5
kamranhossain <kamran.hossain227@gmail.com>
2020-06-29 18:10:44 +0600

    add PlusIcon component into NewTodoButton

--- assets/js/TodoApp/NewTodoButton.tsx
+++ assets/js/TodoApp/NewTodoButton.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import PlusIcon from "./PlusIcon";
+
 interface Props {}
 
 const NewTodoButton = ({}: Props) => {
-  return <button className="new_todo_button">New Reminder</button>;
+  return (
+    <button className="new_todo_button">
+      <span className="new_todo_button__icon">
+        <PlusIcon />
+      </span>
+      New Reminder
+    </button>
+  );
 };
 
 export default NewTodoButton;
